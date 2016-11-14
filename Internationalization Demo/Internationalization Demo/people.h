ea077c08a931057a5b4956dbfa98fa63b671185b
haven <haven@havendeMac.local>
2016-11-14 22:06:25 +0800

    haven
    git客户端的提交

---
+++ Internationalization Demo/Internationalization Demo/people.h
@@ -0,0 +1,13 @@
+//
+//  people.h
+//  Internationalization Demo
+//
+//  Created by haven on 16/11/14.
+//  Copyright © 2016年 haven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface people : NSObject
+
+@end
---
+++ Internationalization Demo/Internationalization Demo/people.m
@@ -0,0 +1,13 @@
+//
+//  people.m
+//  Internationalization Demo
+//
+//  Created by haven on 16/11/14.
+//  Copyright © 2016年 haven. All rights reserved.
+//
+
+#import "people.h"
+
+@implementation people
+
+@end
